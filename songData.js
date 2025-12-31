c4a9565932f7f4d64a99b6889385edcede240682
buu152 <minerubatonnsaga@me.com>
2026-01-01 00:47:25 +0900

    Add files via upload

---
+++ songData.js
@@ -0,0 +1,474 @@
+const songData = {
+  "13.0": [
+    "Absolute Disintegration",
+    "Igallta",
+    "Eat.Wav",
+    "Cuvism³",
+    "Crossing Dimension",
+    "Steer for Next Planet",
+    "OMG",
+    "work^work^work^work^work^work",
+    "Strike the Despair",
+    "Thear",
+    "To our sky",
+    "DiSORDΣR",
+    "Peroizor³",
+    "キユビック",
+    "Awaken In Ruins",
+    "ReVEAL"
+  ],
+
+  "13.1": [
+    "3.566×10^80m³",
+    "Sadalmelik",
+    "Distorted Fate",
+    "Enemy Controller",
+    "FIRST PROMIZE",
+    "Evil Emperor",
+    "Arousal Legato",
+    "Ymir",
+    "This is Jack",
+    "GOODBOUNCE",
+    "GOODMEN",
+    "devil¿la6ue"
+  ],
+
+  "13.2": [
+    "Clockwork Paradox",
+    "飈",
+    "A W A K E",
+    "conflict",
+    "Secret Order",
+    "神々の住まう天域",
+    "LiVEMeTAL",
+    "Wütender Postbote",
+    "TokyoKawaiiBeats",
+    "ZODIAC",
+    "NO ONE YES MAN",
+    "OCCHOCO-REST-LESS",
+    "嵐柳",
+    "Wütender Postbote",
+    "GOODWORLD"
+  ],
+
+  "13.3": [
+    "Floor of Lava (TAKUMI³Edit)",
+    "iL-Artifact",
+    "a truth seeker",
+    "Tera I/O",
+    "BlueWish",
+    "NightTheater",
+    "Hello LimitBreak³r!!!",
+    "Dark Aspect",
+    "DeusX-Machina",
+    "Outrage",
+    "Faith of Eastward",
+    "Roll over Cubic"
+  ],
+
+  "13.4": [
+    "Cryout",
+    "黎明の響 -Echo of Dawn-",
+    "HARD NOTES RUSH!",
+    "SILASIM",
+    "Stratoscraper",
+    "Get Ready!!",
+    "Meteorite",
+    "Claimh",
+    "Drop my DAW"
+  ],
+
+  "13.5": [
+    "いざ、参ります",
+    "!nterroban(?",
+    "Punishment",
+    "階段練習曲",
+    "Oceanus",
+    "bpm160の練習曲",
+    "Finixtahv",
+    "Obscuritas",
+    "OMG",
+    "望郷のオデッセイ",
+    "HATEN CODE 583"
+  ],
+
+  "13.6": [
+    "Marble Rush!n'",
+    "Noesis",
+    "ZEUS",
+    "Toytown",
+    "No-name Requiem",
+    "Bright Star Tactics",
+    "RENDA JOCEKY",
+    "Bright red hertz",
+    "Brain Power",
+    "RESISTANCE",
+    "Shrinewing",
+    "devil¿la6ue",
+    "Unshakable"
+  ],
+
+  "13.7": [
+    "FREEDOM DiVE↓",
+    "≠ One Eleven",
+    "Fafnir",
+    "Do You Remember?",
+    "刹空月想",
+    "Get Out of This",
+    "Cyber Entrance",
+    "Sunset Toybox",
+    "Flamewall",
+    "ReVEAL",
+    "Banbard's Summer",
+    "Luminescence"
+  ],
+
+  "13.8": [
+    "TRⅢDiM3NXi0NaL",
+    "wavelite",
+    "rest^rest^rest^rest^rest^rest",
+    "Signal to the comet",
+    "LiquidSplinter",
+    "Azzurro",
+    "OЯDIN",
+    "As the Wind Dictates",
+    "タマモッチ",
+    "テウメッサ",
+    "Yabai-Jan",
+    "Power Attack",
+    "destiny",
+    "Crowning End",
+    "Strike the Despair",
+    "マジカルラテアート",
+    "ELIMINATE LOCKED"
+  ],
+
+  "13.9": [
+    "NOTES RUSH!",
+    "アイリス・アウトでさようなら",
+    "Better Graphic Animation",
+    "Oversleeping",
+    "Psyched Fevereiro",
+    "Canon(trr⇔don's Remix)",
+    "INFiNiTE ENERZY -Overdoze-",
+    "Back Out",
+    "天Q.",
+    "Fulgurite",
+    "バンバード ~Piano Version~"
+  ],
+
+  "14.0": [
+    "Ops:Limone -FINAL EX-",
+    "Galaxy Shot",
+    "解脱",
+    "Tokyo Haze",
+    "Grayscale Tragedy",
+    "ローテック・ライフ",
+    "Outrage",
+    "DiSORDΣR",
+    "LiVEMeTAL",
+    "This is Jack",
+    "Open The Gate",
+    "ZODIAC"
+  ],
+
+  "14.1": [
+    "Traxxendenza",
+    "Cipher:/2&//<10",
+    "Judge the World",
+    "電脳戦姫 -cyber valkyrie-",
+    "Get Out of This (Remix)",
+    "Connection Destination：Utopia (Remix)",
+    "ParadiseWORLD",
+    "Requillio",
+    "ミラクルサマータイム",
+    "Alice Witch Of Sweets"
+  ],
+
+  "14.2": [
+    "#LOOP",
+    "創世",
+    "Grayed Out-Antifront-",
+    "Drop my DAW",
+    "VEZZELiX",
+    "Iris/Re:construct",
+    "Dutedimpianekepusaan",
+    "Europa the Beauty",
+    "Marionette Lady",
+    "Valmung",
+    "Unshakable",
+    "Gravisphere Crisis",
+    "Echoes of the Forest",
+    "Cybernetic Vampire",
+    "かきくけこーしょん?",
+    "RAKUGAKI NOTE",
+    "GOODBOUNCE",
+    "Rush-More"
+  ],
+
+  "14.3": [
+    "Igallta",
+    "MAGiADR1V3",
+    "Memory：ZERΘ",
+    "Re End of a Dream",
+    "From Nought",
+    "[410]:Odyssey",
+    "宵焔桜火",
+    "花火のための幻想曲",
+    "Rainy/Storm",
+    "Dance with Silence",
+    "バンバード ~FM Shock Version~",
+    "Dizzolve",
+    "VS. マスターコア",
+    "竹?",
+    "Secret Order",
+    "Vapula Cathedral",
+    "Faith of Eastward",
+    "NO ONE YES MAN",
+    "Better Graphic Animation",
+    "N3ON"
+  ],
+
+  "14.4": [
+    "Absolute Disintegration",
+    "Maze",
+    "ZEUS+1 -2017 Flashback-",
+    "Twinkle☆Dreamer",
+    "Dogbite",
+    "Edgelord",
+    "LiR Acanthus",
+    "銀世廻 -Alternative-",
+    "Starry Sky",
+    "Rubbish Sorting",
+    "Awaken In Ruins",
+    "ST4R DUST"
+  ],
+
+  "14.5": [
+    "Wheel of 4 Tune",
+    "Stratostrider",
+    "Fxxking Wedding",
+    "Electrick or Treat",
+    "Reflect RGB",
+    "Grasshopper",
+    "ニニ",
+    "NightTheater",
+    "bpm160の練習曲",
+    "OCCHOCO-REST-LESS",
+    "席替えやったー！"
+  ],
+
+  "14.6": [
+    "maessigkeit",
+    "G e n g a o z o -45",
+    "αthena +35t",
+    "天照らす律動",
+    "She has gone out 0f reach",
+    "3LiMiNATE",
+    "rvLer",
+    "ViCToRi4",
+    "Dead AND Alive!",
+    "Espace neige",
+    "Moonstone?",
+    "Enemy Controller",
+    "Obscuritas"
+  ],
+
+  "14.7": [
+    "NiFlhel",
+    "+η",
+    "tundra",
+    "Connection Destination Utopia",
+    "Aquae Et Puella",
+    "Memory Of Summer Days",
+    "Stratoscraper",
+    "Arousal Legato",
+    "Are You Ready???",
+    "Daydream",
+    "Luminescence",
+    "Brain Power",
+    "電脳戦姫 -cyber valkyrie-",
+    "Flashdance"
+  ],
+
+  "14.8": [
+    "キマグレ☆テキトウ・アンサンブル→コミュニケーション",
+    "ULTRARENDA1",
+    "Still Be, Will Be",
+    "A W A K E",
+    "Couleur",
+    "Eltaw",
+    "tundra",
+    "YRU",
+    "タマモッチ",
+    "Erwachen(TAKUMI³ mix)"
+  ],
+
+  "14.9": [
+    "ZEUS 666",
+    "Squall - TAKUMIX -",
+    "Yggdrasill",
+    "SO GRAND.",
+    "Naughty Naughty Angel",
+    "Last Noble Flame",
+    "BPM = BPM + 1",
+    "泡沫のジェミニ",
+    "work^work^work^work^work^work",
+    "Sunset Toybox",
+    "テウメッサ",
+    "Finixtahv",
+    "Marionette Lady"
+  ],
+
+  "15.0": [
+    "ARCANAA?",
+    "BPM=RT",
+    "Creigis",
+    "RESURRECTION WORLD",
+    "KiNG FORM -XIII-",
+    "G e n g a o z o -45",
+    "LiquidSplinter",
+    "ZEUS",
+    "Thear",
+    "解脱",
+    "Connection Destination：Utopia",
+    "OMG"
+  ],
+
+  "15.1": [
+    "Clockwork Paradox",
+    "紅恋桜",
+    "Sephirah×ClusteR",
+    "GOODWORLD",
+    "Ops:Limone -FINAL EX-",
+    "階段練習曲",
+    "琴音",
+    "Bright Star Tactics",
+    "GOODMEN",
+    "Banbard's Summer",
+    "キユビック",
+    "OMG",
+    "Power Attack",
+    "Dance with Silence"
+  ],
+
+  "15.2": [
+    "3.566×10^80m³",
+    "A EaGLE",
+    "≠ One Eleven",
+    "Distorted Fate",
+    "Fafnir",
+    "透るエデンに花束を",
+    "Cryout",
+    "ZEUS 666",
+    "Peroizor³",
+    "Back Out",
+    "Eltriwm",
+    "天Q.",
+    "Bright red hertz",
+    "HATEN CODE 583",
+    "Sephirah×ClusteR"
+  ],
+
+  "15.3": [
+    "TRⅢDiM3NXi0NaL",
+    "FREEDOM DiVE↓",
+    "Floor of Lava (TAKUMI³Edit)",
+    "Traxxendenza",
+    "Maze",
+    "!nterroban(?",
+    "ミルキーウェイに口づけを",
+    "Taiko Warrior",
+    "ORDIN -Apocalyptic War-",
+    "INFiNiTE ENERZY -Overdoze-",
+    "BPM = BPM + 1",
+    "竹?"
+  ],
+
+  "15.4": [
+    "Melody to Heaven",
+    "Taiko Warrior",
+    "Open The Gate",
+    "Cuvism³",
+    "Mr.RENDA",
+    "WOLVRAVES",
+    "Oversleeping",
+    "神々の住まう天域",
+    "ParadiseWORLD",
+    "RENDA JOCEKY",
+    "アイリス・アウトでさようなら",
+    "BPM=RT"
+  ],
+
+  "15.5": [
+    "iL-Artifact",
+    "Eat.Wav",
+    "Grayed Out-Antifront-",
+    "Evil Emperor",
+    "Dizzolve",
+    "VEZZELiX",
+    "Igallta",
+    "嵐柳"
+  ],
+
+  "15.6": [
+    "KERBEROX",
+    "Wheel of 4 Tune",
+    "Requillio",
+    "Ymir",
+    "Twinkle☆Dreamer",
+    "Flamewall",
+    "Valmung"
+  ],
+
+  "15.7": [
+    "夜襲",
+    "volcanic",
+    "Crossing Dimension",
+    "From Nought",
+    "No-name Requiem",
+    "Cuvism³",
+    "Edgelord",
+    "VS. マスターコア",
+    "Cybernetic Vampire"
+  ],
+
+  "15.8": [
+    "Sadalmelik",
+    "Tera I/O",
+    "DENGEKI Tube",
+    "She has gone out 0f reach",
+    "花火のための幻想曲"
+  ],
+
+  "15.9": [
+    "a truth seeker",
+    "飈",
+    "Absolute Disintegration",
+    "Floor of Lava (TAKUMI³Edit)",
+    "Gravisphere Crisis",
+    "Distorted Fate",
+    "Dutedimpianekepusaan"
+  ],
+
+  "16.0": [
+    "TRⅢDiM3NXi0NaL",
+    "Clockwork Paradox",
+    "conflict",
+    "≠ One Eleven",
+    "Stratostrider"
+  ],
+
+  "16.1": [
+    "NOTES RUSH!",
+    "3.566×10^80m³",
+    "FREEDOM DiVE↓",
+    "Re：End of a Dream"
+  ],
+
+  "16.2": [],
+
+  "16.3": [
+    "Traxxendenza"
+  ]
+};
